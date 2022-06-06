9be6894fabea8716b80da67067e7acc661e518c1
Carlos Moura <carlosmourapires88@gmail.com>
2022-06-06 01:45:42 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+SELECT us.`name` AS usuario, COUNT(hi.song_id) AS qtda_musicas_ouvidas, ROUND(SUM(so.seconds)/60, 2) AS total_minutos
+FROM SpotifyClone.users AS us
+INNER JOIN SpotifyClone.historic AS hi
+ON us.user_id = hi.user_id
+INNER JOIN SpotifyClone.songs AS so
+ON so.song_id = hi.song_id
+GROUP BY usuario;
