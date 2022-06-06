88a6dcba9937b6f888c986dc4de88b6bd58333ed
Carlos Moura <carlosmourapires88@gmail.com>
2022-06-06 01:46:38 -0300

    Correção sintaxe desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
-SELECT us.`name` AS usuario, COUNT(hi.song_id) AS qtda_musicas_ouvidas, ROUND(SUM(so.seconds)/60, 2) AS total_minutos
+SELECT us.`name` AS usuario, COUNT(hi.song_id) AS qtde_musicas_ouvidas, ROUND(SUM(so.seconds)/60, 2) AS total_minutos
 FROM SpotifyClone.users AS us
 INNER JOIN SpotifyClone.historic AS hi
 ON us.user_id = hi.user_id
